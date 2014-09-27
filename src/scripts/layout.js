d410c6c280e7e50a4c77d3f81ae82b3f007fc52e
Mark Wise <m2r5.w4s3@gmail.com>
2014-09-26 22:23:18 -0500

    refactored and added comments to layout.js

--- src/scripts/layout.js
+++ src/scripts/layout.js
@@ -9,47 +9,54 @@ keyStore,
 styleSheet
 */
 
-//requires: supports_media_queries
-//requires: generate_agid
-//requires: key_store
-//requires: attr_list
-//requires: media_match
+//
+// @module
+//
+// Handles all operations related to an ag layout element
+//
 
 var layout = (function () {
-    'use strict';
-    
-    var removeModifiers = function (ele) {
-		var agid = ele.agid,
-			store = keyStore.get(agid);
-		
-		store.responding = true;
-		attr('ag', ele).remove();
-		attr('ag-reflow', ele).set();
-		styleSheet.clear(agid);
-		attr('ag-reflow', ele).remove();
-	};
-
-
-	var applyModifiers = function (ele) {
-		var store = keyStore.get(ele.agid);
-		store.responding = false;
-		attr('ag', ele).set(store.modifiers);
-	};
-
-
-	var getContentMaxHeight = function (ele) {
-		var store = keyStore.get(ele.agid),
-			eles = store.eles,
+	'use strict';
+	
+	//
+	// @private
+	//
+	// Returns the largest offsetHeight value of all ag-cel elements to be used
+	// as the min-height. As the method name implies, this is for IE8 only.
+	//
+	// @param {HTMLElement} ele
+	//		An ag element
+	//
+	// @returns {Number}
+	//		The min-height value
+	//
+	//
+	
+	var getContentMaxHeightIE8 = function (ele) {
+		var eles = getVisibleElements(ele),
 			i = eles.length,
 			a = [];
 	
 		while (i--) {
 			a.push(eles[i].offsetHeight);
 		}
-	
+		
 		return Math.max.apply(null, a);
 	};
-
+	
+	
+	//
+	// @private
+	//
+	// Returns the height of an ag element that will be used as the min-height
+	// value for all ag-cel and ag-line elements
+	//
+	// @param {HTMLElement} ele
+	//		An ag element
+	//
+	// @returns {Number}
+	//		The min-height value
+	//
 
 	var getMinHeight = function (ele) {
 		var style = win.getComputedStyle,
@@ -64,64 +71,31 @@ var layout = (function () {
 			height = ele.currentStyle.height;
 		
 			if (height === 'auto') {
-				height = getContentMaxHeight(ele) + 'px';
+				height = getContentMaxHeightIE8(ele) + 'px';
 			}
 		}
 	
 		return height;
 	};
 
-
-	var resizeMinHeight = function (ele) {
-		var agid = ele.agid,
-			store = keyStore.get(agid),
-			styles;
 	
-		if (store.responding) {
-			return;
-		}
+	//
+	// @private
+	//
+	// Creates and returns an ag-line element
+	//
+	// @params {Number} index
+	//		The index of the line in relation to other lines in a layout. This 
+	//		index is used as a styling hook to show and hide ag-line elements.
+	//
+	// @returns {HTMLElement}
+	//		An ag-line element
+	//
 	
-		attr('ag-reflow', ele).set();
-	
-		styles = [
-			'[ag-id="', agid, '"] > [ag-line] {\n',
-				//jshint -W015
-				'\tmin-height: ', getMinHeight(ele), ';\n',
-			'}'
-		].join('');
-	
-		if (!store.hasAlignModifier) {
-			styles = ['[ag-id="', agid, '"] > [ag-cel],\n', styles].join('');
-		}
-	
-		styleSheet.set(agid, styles);
-		attr('ag-reflow', ele).remove();
-	};
-
-
-	var initializeResponsiveLayout = function (ele) {
-		if (!supportsMediaQueries) {
-			return;
-		}
-	
-		var width = attr('ag-res', ele).get(),
-			media;
-	
-		if (width) {
-			media = 'screen and (max-width:' + width + 'px)';
-
-			if (mediaMatch(media).matches) {
-				removeModifiers(ele);
-			}
-		}
-	};
-
-
-	var createLine = function (lines, index) {
+	var createLine = function (index) {
 		var fragment = doc.createDocumentFragment(),
 			line = doc.createElement('div');
 	
-		lines.push(line);
 		attr('ag-line', line).set(index);
 		//Safari requires a tab character
 		fragment.appendChild(doc.createTextNode('\u0009'));
@@ -132,15 +106,18 @@ var layout = (function () {
 	};
 
 
-	var createLines = function (ele, cels, lines) {
-		var eles = cels.slice(1),
-			i = eles.length;
-	
-		while (i--) {
-			ele.insertBefore(createLine(lines, i + 1), eles[i]);
-		}
-	};
-
+	//
+	// @private
+	//
+	// Returns all visible ag-cel elements unless they have the show modifier,
+	// which are hidden by default when not responding.
+	//
+	// @param {HTMLElement} ele
+	//		An ag element
+	//
+	// @returns {Array}
+	//		A list of all visible ag-cel elements
+	//
 
 	var getVisibleElements = function (ele) {
 		var node = ele.firstChild,
@@ -160,6 +137,64 @@ var layout = (function () {
 	};
 
 
+	//
+	// @private
+	//
+	// Respond gets called once during intialization to see if a layout is
+	// responding. The ag directive is then removed from the layout effectively
+	// removing all styles leaving a clean style definition for mobile and
+	// tablet devices.
+	//
+	// @param {HTMLElement} ele
+	//		An ag element
+	//
+
+	var respond = function (ele) {
+		if (!supportsMediaQueries) {
+			return;
+		}
+	
+		var width = attr('ag-res', ele).get(),
+			media;
+	
+		if (width) {
+			media = 'screen and (max-width:' + width + 'px)';
+
+			if (mediaMatch(media).matches) {
+				removeModifiers(ele);
+			}
+		}
+	};
+
+	
+	//
+	// @private
+	//
+	// Creates ag-line elements
+	//
+	// @param {HTMLElement} ele
+	//		An ag element
+	//
+
+	var createLines = function (ele) {
+		var eles = getVisibleElements(ele).slice(1),
+			i = eles.length;
+	
+		while (i--) {
+			ele.insertBefore(createLine(i + 1), eles[i]);
+		}
+	};
+
+	
+	//
+	// @private
+	//
+	// As the name implies, all whitespace nodes are removed from an ag element
+	//
+	// @param {HTMLElement} ele
+	//		An ag element
+	//
+	
 	var removeWhitespace = function (ele) {
 		var node = ele.firstChild,
 			next;
@@ -174,66 +209,211 @@ var layout = (function () {
 			node = next;
 		}
 	};
+	
+	
+	//
+	// @private
+	//
+	// Does the ag directive contain a flip modifier
+	//
+	// @param {String} modifiers
+	//		An ag directives modifiers
+	//
+	// @returns {Boolen}
+	//		True or false if the modifier exists
+	//
 
-
-	var hasFlipModifier = function (ele, modifiers) {
+	var hasFlipModifier = function (modifiers) {
 		return /(?:^|\s+)flip(?:\s+|$)/.test(modifiers);
 	};
 
+	
+	//
+	// @private
+	//
+	// Does the ag directive contain a lines modifier
+	//
+	// @param {String} modifiers
+	//		An ag directives modifiers
+	//
+	// @returns {Boolen}
+	//		True or false if the modifier exists
+	//
+	
+	var hasLinesModifier = function (modifiers) {
+		return /(?:^|\s+)lines(?::(?:show|hide):\d)?(?:\s+|$)/.test(modifiers);
+	};
+	
+	
+	//
+	// @private
+	//
+	// Does the ag directive contain a align modifier
+	//
+	// @param {String} modifiers
+	//		An ag directives modifiers
+	//
+	// @returns {Boolen}
+	//		True or false if the modifier exists
+	//
 
-	var hasAlignModifier = function (ele, modifiers) {
+	var hasAlignModifier = function (modifiers) {
 		return /(?:^|\s+)align:[tmb](?:\s+|$)/.test(modifiers);
 	};
 
-
-	var hasSpaceModifier = function (ele, modifiers) {
+	
+	//
+	// @private
+	//
+	// Does the ag directive contain a space modifier
+	//
+	// @param {String} modifiers
+	//		An ag directives modifiers
+	//
+	// @returns {Boolen}
+	//		True or false if the modifier exists
+	//
+	
+	var hasSpaceModifier = function (modifiers) {
 		return /(?:^|\s+)space:[12345](?:\s+|$)/.test(modifiers);
 	};
+	
+	
+	//
+	// @private
+	//
+	// Does the ag directive contain a split modifier
+	//
+	// @param {String} modifiers
+	//		An ag directives modifiers
+	//
+	// @returns {Boolen}
+	//		True or false if the modifier exists
+	//
 
-
-	var hasSplitModifier = function (ele, modifiers) {
+	var hasSplitModifier = function (modifiers) {
 		return /(?:^|\s+)split:(?:[2345]|[1]\/[23]|[23]\/1|2\/3|3\/2)(?:\s+|$)/.test(modifiers);
 	};
 
+	
+	//
+	// @public
+	//
+	// Sets the min-height for all ag-cel and ag-line elements. If the align
+	// modifier exists, only ag-line elements get a min-height value set.
+	// 
+	// @param {HTMLElement} ele
+	//		An ag element
+	//
 
-	var hasLineModifier = function (ele, modifiers) {
-		return /(?:^|\s+)lines(?::(?:show|hide):\d)?(?:\s+|$)/.test(modifiers);
+	var resizeMinHeight = function (ele) {
+		var agid = ele.agid,
+			store = keyStore.get(agid),
+			styles;
+	
+		if (store.responding) {
+			return;
+		}
+	
+		//Sets the min-height value to 0 for all ag-cel and ag-line elements
+		//while a new min-height value is being calculated
+		attr('ag-reflow', ele).set();
+	
+		styles = [
+			'[ag-id="', agid, '"] > [ag-line] {\n',
+				//jshint -W015
+				'\tmin-height: ', getMinHeight(ele), ';\n',
+			'}'
+		].join('');
+		
+		//Only include ag-cel elements if the align modifier is not present
+		if (!store.hasAlignModifier) {
+			styles = ['[ag-id="', agid, '"] > [ag-cel],\n', styles].join('');
+		}
+	
+		styleSheet.set(agid, styles);
+		attr('ag-reflow', ele).remove();
+	};
+	
+	
+	//
+	// @public
+	//
+	// Restores the ag directive and stored modifiers back to a layout
+	// 
+	// @param {HTMLElement} ele
+	//		An ag element
+	//
+
+	var applyModifiers = function (ele) {
+		var store = keyStore.get(ele.agid);
+	
+		store.responding = false;
+		attr('ag', ele).set(store.modifiers);
+	};
+
+	
+	//
+	// @public
+	//
+	// Removes the ag directive and all modifiers from a layout
+	// 
+	// @param {HTMLElement} ele
+	//		An ag element
+	//
+	
+	var removeModifiers = function (ele) {
+		var agid = ele.agid,
+			store = keyStore.get(agid);
+	
+		store.responding = true;
+		attr('ag', ele).remove();
+		attr('ag-reflow', ele).set();
+		styleSheet.clear(agid);
+		attr('ag-reflow', ele).remove();
 	};
 
 
+	//
+	// @public
+	//
+	// The core layout initialization method. This is where all the good stuff
+	// happens
+	// 
+	// @param {HTMLElement} ele
+	//		An ag element
+	//
+	
 	var initialize = function (ele) {
 
 		if (ele.agid !== void 0) {
 			return;
 		}
-	
+
 		var agid = generateAgId(ele),
 			store = keyStore.create(agid),
 			modifiers;
-	
-		store.responding = false;
+		
 		store.modifiers = modifiers = attr('ag', ele).get();
-		store.hasSplitModifier = hasSplitModifier(ele, modifiers);
-		store.hasAlignModifier = hasAlignModifier(ele, modifiers);
-		store.hasSpaceModifier = hasSpaceModifier(ele, modifiers);
-		store.hasFlipModifier = hasFlipModifier(ele, modifiers);
-		store.hasLineModifier = hasLineModifier(ele, modifiers);
+		store.hasSplitModifier = hasSplitModifier(modifiers);
+		store.hasSpaceModifier = hasSpaceModifier(modifiers);
+		store.hasAlignModifier = hasAlignModifier(modifiers);
+		store.hasLinesModifier = hasLinesModifier(modifiers);
+		store.hasFlipModifier = hasFlipModifier(modifiers);
+
 		styleSheet.create(agid);
 		removeWhitespace(ele);
-		store.cels = getVisibleElements(ele);
-		store.lines = [];
-		createLines(ele, store.cels, store.lines);
-		store.eles = getVisibleElements(ele);
-		initializeResponsiveLayout(ele);
+		createLines(ele);
+		respond(ele);
 		resizeMinHeight(ele);
 		attr('ag-ready', ele).set();
 	};
 
 
 	return {
+		initialize: initialize,
 		removeModifiers: removeModifiers,
 		applyModifiers: applyModifiers,
-		initialize: initialize,
 		resizeMinHeight: resizeMinHeight
 	};
 }());
